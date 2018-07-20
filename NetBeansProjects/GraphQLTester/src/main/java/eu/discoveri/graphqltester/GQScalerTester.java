d35205a3a23069da5d6ed71b9be5a8c1fb0100ef
chrispowell <chrispowell@chrisp-tnp-PC1>
2018-07-20 23:08:33 +0200

    Initial

--- NetBeansProjects/GraphQLTester/src/main/java/eu/discoveri/graphqltester/GQScalerTester.java
+++ NetBeansProjects/GraphQLTester/src/main/java/eu/discoveri/graphqltester/GQScalerTester.java
@@ -6,44 +6,34 @@ import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.GraphQLError;
-import static graphql.Scalars.GraphQLID;
-import static graphql.Scalars.GraphQLString;
 import graphql.TypeResolutionEnvironment;
-
 import graphql.schema.DataFetcher;
-import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
-import graphql.schema.GraphQLList;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.GraphQLObjectType;
-import static graphql.schema.GraphQLObjectType.newObject;
-import graphql.schema.GraphQLTypeReference;
 import graphql.schema.TypeResolver;
 import graphql.schema.idl.EnumValuesProvider;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
-
 import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
-import graphql.servlet.SimpleGraphQLServlet;
 
 import java.io.File;
 import java.util.List;
-import javax.servlet.annotation.WebServlet;
 
 
 /**
  *
  * @author chrispowell
  */
-@WebServlet("/astrology")
-public class GQTest411 extends SimpleGraphQLServlet.Builder
+public class GQScalerTester
 {
-    public GQTest411(GraphQLSchema schema) {
-        super(schema);
-    }
-
     private static final EnumValuesProvider LUResolver = LengthUnit::valueOf;
+    private static final DataFetcher p1DataFetcher = new P1DataFetcher();
+    
+    /*
+     * Entity resolver
+     */
     private static TypeResolver resolveEntity()
     {
         return (TypeResolutionEnvironment env) -> {
@@ -51,53 +41,23 @@ public class GQTest411 extends SimpleGraphQLServlet.Builder
             return null;    
         };
     }
-    private static final DataFetcher signDataFetcher = new SignDataFetcher();
-    private static final DataFetcher personDataFetcher = new PersonDataFetcher();
     
+    /*
+     * Build the schema
+     */
     private static GraphQL schemaBuild()
     {
         SchemaParser schemaParser = new SchemaParser();
         SchemaGenerator schemaGenerator = new SchemaGenerator();
         
         File schemaFile = new File("/home/chrispowell/NetBeansProjects/GraphQLTester/src/main/java/resources/AstroSchemaHalf.graphqls");
-        
-        // Test schema
-//        # Person
-//        type Person implements Entity {
-//            # ID
-//            id: ID!
-//            # identifier
-//            identifier: String
-//            # name (username)
-//            name: String!
-//            # DoB
-//            DoB: DateTime
-//        }
-//        GraphQLObjectType person = newObject()
-//                .name("Person")
-//                .description("This is a Person object")
-//                .field(newFieldDefinition().name("id")
-//                        .type(GraphQLID).build())
-//                .field(newFieldDefinition().name("identifier")
-//                        .type(GraphQLString).build())
-//                .field(newFieldDefinition().name("name")
-//                        .type(GraphQLString).build())
-//                .field(newFieldDefinition().name("DoB")
-//                        .type(CustomScalar.ZONEDDATETIME).build()).build();
-//                .field(newFieldDefinition().name("friends")
-//                    .type(GraphQLList.list(GraphQLTypeReference.typeRef("Person")))).build();
-        
+       
         RuntimeWiring wiring = newRuntimeWiring()
                 .scalar(CustomScalar.ZONEDDATETIME)
                 .type("Entity",typeWiring->typeWiring.typeResolver(resolveEntity()))
-//                .type("LengthUnit",typeWiring->typeWiring.enumValues(LUResolver))
                 .type("QueryEndPoint", typeWiring -> typeWiring
-                    .dataFetcher("person", personDataFetcher))
-//                    .dataFetcher("sign", signDataFetcher))
+                    .dataFetcher("person", p1DataFetcher))
                 .build();
-        
-        /* Remove */
-        //wiring.getScalars().forEach((k,v)->System.out.println("...> Key: " +k+ ", Val: " +v.getName()));
         
         TypeDefinitionRegistry typeRegistry = schemaParser.parse(schemaFile);
         GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
@@ -105,11 +65,15 @@ public class GQTest411 extends SimpleGraphQLServlet.Builder
         return GraphQL.newGraphQL(graphQLSchema).build();
     }
     
+    /*
+     * Run a query
+     */
     private static ExecutionResult runQuery( GraphQL graphQL, String query )
     {
         ExecutionInput execInput = ExecutionInput.newExecutionInput().query(query).build();
         return graphQL.execute(execInput);
     }
+
     
     /**
      * M A I N
@@ -133,26 +97,9 @@ public class GQTest411 extends SimpleGraphQLServlet.Builder
         else
         {
             System.out.println("***Errors:");
-            // ** Do this with functional...
             errors.forEach((gqlErr) -> {
                 System.out.println("  " +gqlErr.getMessage());
             });
         }
-        
-        // Get a ZodiacSign
-//        execResult = runQuery( graphQL, "{\nsign(id: 2){\nid\n name\n}\n}" ); // {\nastrology(id:9){\nname\n signs{\nid\n name\n}\n}\n}
-//        errors = execResult.getErrors();
-//        if( errors.isEmpty() )
-//        {
-//            Object signData = execResult.getData();
-//            System.out.println("JSON>>> " +signData);
-//        }
-//        else
-//        {
-//            System.out.println("***Errors:");
-//            // ** Do this with functional...
-//            for( GraphQLError gqlErr: errors )
-//                { System.out.println("  " +gqlErr.getMessage()); }
-//        }
     }
 }
---
+++ NetBeansProjects/GraphQLTester/src/main/java/eu/discoveri/graphqltester/P1DataFetcher.java
@@ -0,0 +1,24 @@
+/*
+ */
+package eu.discoveri.graphqltester;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+
+/**
+ *
+ * @author chrispowell
+ */
+public class P1DataFetcher implements DataFetcher
+{
+    // This'll be a database fetch sometime...
+    @Override
+    public Person get(final DataFetchingEnvironment dataFE)
+    {
+        return new Person(UUID.randomUUID(),"EH","Edwin Hubble",ZonedDateTime.parse("2007-12-03T10:15:30+01:00[Europe/Paris]"));
+    }
+}
