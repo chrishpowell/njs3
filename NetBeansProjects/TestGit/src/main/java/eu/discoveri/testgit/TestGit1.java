f5f8830460c5595634ecddb495c7b3fd19a66220
chrispowell <chrispowell@chrisp-tnp-PC1>
2020-03-04 08:58:39 +0200

    TestGit

---
+++ NetBeansProjects/TestGit/src/main/java/eu/discoveri/testgit/TestGit1.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) Discoveri SIA - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package eu.discoveri.testgit;
+
+/**
+ *
+ * @author Chris Powell, Discoveri OU
+ * @email info@astrology.ninja
+ */
+public class TestGit1
+{
+
+}
